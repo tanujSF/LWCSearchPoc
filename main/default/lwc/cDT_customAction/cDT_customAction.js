de9d2b74ba0c92459722888b07b5b621807f4e75
TanujTyagiGS <ttyagi@rodanandfields.com>
2021-06-02 02:52:19 -0700

    Kebab case Naming

--- main/default/lwc/cDT_customAction/cDT_customAction.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CDT_customAction extends LightningElement {}
--- main/default/lwc/cDT_customDataTable/cDT_customDataTable.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class CDT_customDataTable extends LightningElement {}

c0e4b5a2b86efa9f3f31473ced1b1f1910f207b9
TanujTyagiGS <ttyagi@rodanandfields.com>
2021-06-06 10:20:55 -0700

    Custom Lookup Column

--- main/default/lwc/cdtCustomDataTableDemo/cdtCustomDataTableDemo.js
+++ main/default/lwc/cdtCustomDataTableDemo/cdtCustomDataTableDemo.js
@@ -2,8 +2,15 @@ import { LightningElement, track } from 'lwc';
 
 export default class cdtCustomDataTableDemo extends LightningElement {
     
-    columns = [
-        { label: 'Name', fieldName: 'Name', editable: true},
+    @track data = [];
+    //have this attribute to track data changed
+    //with custom picklist or custom lookup
+    @track draftValues = [];
+
+    lastSavedData = [];
+    connectedCallback() {
+    this.columns = [
+        { label: 'Name', fieldName: 'Name'},
         { label: 'Custom Button', fieldName: 'id', type: 'customAction', typeAttributes: {
             customButtonValue: { fieldName: 'customField' }
             }
@@ -20,18 +27,67 @@ export default class cdtCustomDataTableDemo extends LightningElement {
     }
     ];
 
-    @track data = [
+     this.data = [
         { id: 1, Name: 'Burlington Textiles', customField: 1, Rating: 'Hot'},
         { id: 2, Name: 'Ab Corp & Gas, UK', customField: 2, Rating: 'Cold'},
         { id: 3, Name: 'Alpha Dynamics', customField: 3, Rating: 'Hot'},
         { id: 4, Name: 'Northern Trail Travel', customField: 4, Rating: 'Cold'}
 
     ];
-    
+
+    this.lastSavedData = JSON.parse(JSON.stringify(this.data));
+
+}
     handleCustomButtonAction(event) {
         const { recordId, newcustomButtonValue } = event.detail;
         console.log('CUSTOM Button Field' + recordId + ' - ' + newcustomButtonValue);
         this.data.find(item => item.id == recordId).customField = newcustomButtonValue;
         this.data = [...this.data];
+    }
+
+    updateDataValues(updateItem) {
+        let copyData = [... this.data];
+        copyData.forEach(item => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+            }
+        });
+
+        //write changes back to original data
+        this.data = [...copyData];
+    }
+
+    updateDraftValues(updateItem) {
+        let draftValueChanged = false;
+        let copyDraftValues = [...this.draftValues];
+        //store changed value to do operations
+        //on save. This will enable inline editing &
+        //show standard cancel & save button
+        copyDraftValues.forEach(item => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+                draftValueChanged = true;
+            }
+        });
+
+        if (draftValueChanged) {
+            this.draftValues = [...copyDraftValues];
+        } else {
+            this.draftValues = [...copyDraftValues, updateItem];
+        }
+    }
+
+    //listener handler to get the context and data
+    //updates datatable
+    picklistChanged(event) {
+        event.stopPropagation();
+        let dataRecieved = event.detail.data;
+        let updatedItem = { Id: dataRecieved.context, Rating: dataRecieved.value };
+        this.updateDraftValues(updatedItem);
+        this.updateDataValues(updatedItem);
     }
 }
--- main/default/lwc/cdtCustomLookup/cdtCustomLookup.js
+++ main/default/lwc/cdtCustomLookup/cdtCustomLookup.js
@@ -1,3 +1,201 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+/* eslint-disable @lwc/lwc/no-async-operation */
 
-export default class cdtCustomLookup extends LightningElement {}
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { api, LightningElement, track, wire } from 'lwc';
+
+export default class LookupLwc extends LightningElement {
+
+    @api valueId;
+    @api objName = 'Account' ;
+    @api iconName = 'standard:account';
+    @api labelName = 'Account';
+    @api readOnly = false;
+    @api filters = '';
+    @api showLabel = false;
+    @api uniqueKey;
+    @api placeholder = 'Search';
+    @api displayFields = 'Name, AccountNumber';
+    @api displayFormat = 'Name (AccountNumber)';
+    objLabelName;
+
+    /*Create Record Start*/
+    @api createRecord;
+    @track recordTypeOptions;
+    @track createRecordOpen;
+    @track recordTypeSelector;
+    @track mainRecord;
+    @track isLoaded = false;
+
+    @track label;
+    @track options; //lookup values
+    @track isValue;
+    @track blurTimeout;
+    @track defaultValue;
+
+    searchTerm;
+    href;
+    blurTimeout;
+
+    //css
+    @track boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus';
+    @track inputClass = '';
+
+    connectedCallback() {
+        this.defaultValue = this.valueId;
+        if (!this.displayFormat) {
+            let splitFields = this.displayFields.split(',');
+            this.displayFormat = splitFields[0];
+        }
+    }
+
+    renderedCallback() {
+        if (this.objName) {
+            let temp = this.objName;
+            if (temp.includes('__c')) {
+                let newObjName = temp.replace(/__c/g, "");
+                if (newObjName.includes('_')) {
+                    let vNewObjName = newObjName.replace(/_/g, " ");
+                    this.objLabelName = vNewObjName;
+                } else {
+                    this.objLabelName = newObjName;
+                }
+
+            } else {
+                this.objLabelName = this.objName;
+            }
+        }
+    }
+
+    @wire(lookUp, { searchTerm: '$searchTerm', objectName: '$objName', filters: '$filters', fields: '$displayFields' })
+    wiredRecords({ error, data }) {
+        if (data) {
+            this.error = undefined;
+            this.options = [];
+            data.forEach(item => {
+                let option = { ...item };
+                option.label = this.generateLabel(option);
+                this.options.push(option);
+            });
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+    @wire(lookUp, { recordId: '$valueId', objectName: '$objName', fields: '$displayFields' })
+    wiredDefault({ error, data }) {
+        if (data) {
+            if (this.valueId) {
+                this.selectItem(data[0]);
+                this.options = undefined;
+            }
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+    handleClick() {
+        this.searchTerm = '';
+        this.inputClass = 'slds-has-focus';
+        this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open';
+    }
+
+    inblur() {
+        this.blurTimeout = setTimeout(() => { this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus' }, 300);
+    }
+
+    onSelect(event) {
+        let ele = event.currentTarget;
+        let selectedId = ele.dataset.id;
+        let key = this.uniqueKey;
+
+        this.dispatchEvent(new CustomEvent('valueselect', {
+            composed: true,
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                data: { selectedId, key },
+            }
+        }));
+
+        if (this.blurTimeout) {
+            clearTimeout(this.blurTimeout);
+        }
+
+        this.options.forEach(option => {
+            if (option.Id === selectedId) {
+                this.selectItem(option);
+            }
+        });
+    }
+
+    selectItem(record) {
+        //show selection value on screen
+        this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus';
+        this.label = this.generateLabel(record);
+        this.href = '/' + record.Id;
+        this.isValue = true;
+        this.options = undefined;
+    }
+
+    generateLabel(record) {
+        console.log('record', record);
+        let label = this.displayFormat;
+        let splitFields = this.displayFields.split(',');
+        splitFields.forEach(field => {
+            field = field.trim();
+            let value;
+
+            //logic to handle relationhships in queries
+            if (field.indexOf('.') > -1) {
+                let splitRelations = field.split('.');
+                splitRelations.forEach(item => {
+                    value = (value ? value[item] : record[item]);
+                });
+            } else {
+                value = record[field];
+            }
+            label = label.replace(field, value);
+        });
+        return label;
+    }
+
+    onChange(event) {
+        this.searchTerm = event.target.value;
+    }
+
+    handleRemovePill() {
+        this.isValue = false;
+        this.valueId = '';
+        let selectedId = '';
+        let key = this.uniqueKey;
+        this.dispatchEvent(new CustomEvent('valueselect', {
+            composed: true,
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                data: { selectedId, key },
+            }
+        }));
+    }
+
+    handleError() {
+
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: 'Error saving the record',
+                variant: 'error',
+            }),
+        )
+    }
+
+    closeModal() {
+        this.stencilClass = '';
+        this.stencilReplacement = 'slds-hide';
+        this.createRecordOpen = false;
+        this.recordTypeSelector = false;
+        this.mainRecord = false;
+    }
+}
